866c904207b32db3c4937f6672ce06127145d5d8
Roger Robertson Jr <rjrjr@pm.me>
2020-03-21 15:03:10 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+
+    def actors_list
+        Character.all.map {|character| character.actor.first_name + " " + character.actor.last_name}
+    end
 end
