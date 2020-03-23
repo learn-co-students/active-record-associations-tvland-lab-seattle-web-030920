a6f44ab989aeadd2cb2860d69fd9b0ee05c2a23c
Bakugou808 <janu.m.sung@gmail.com>
2020-03-23 15:34:37 -0700

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
-class Character < ActiveRecord::Base
-  
+class Character < ActiveRecord::Base #join tables have singular, AR knows and looks for the pluralization  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{name} always says: #{catchphrase}"
+  end
+
+#   def build_network
+
+#   end
 end
